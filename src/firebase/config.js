ebf764811e7c846db8aa5d4d31b51a03056e731b
sumi54 <sumeyyekarakullukcu34@gmail.com>
2023-08-22 23:44:38 +0300

    gizleme

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 import {initializeApp} from 'firebase/app';
 import { getFirestore,Timestamp} from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyAo94FG5XQr5zjnLGlldY20G--0h_GB7CI",
-    authDomain: "company-registation.firebaseapp.com",
-    projectId: "company-registation",
-    storageBucket: "company-registation.appspot.com",
-    messagingSenderId: "573928954464",
-    appId: "1:573928954464:web:b3e0311b5d08e3538b0105"
+    apiKey: "###",
+    authDomain: "###",
+    projectId: "###",
+    storageBucket: "###",
+    messagingSenderId: "###",
+    appId: "###"
   };
   initializeApp(firebaseConfig)
   const db=getFirestore()
